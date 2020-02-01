b721f935052c36423eb7930aba02998c0619d6a7
Emily Taylor <emilyporterfieldtaylor@gmail.com>
2020-02-01 10:59:26 -0600

    change format of HTML to accommodate APIs

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 $( document ).ready(function() {
   console.log( "ready!" );
 });
+var movieKey = "f186a14b09112b273bdded14d4bbc874";
+var movieURL = "https://api.themoviedb.org/3/movie/now_playing?api_key=" + movieKey + "&language=en-US&page=1&region=USA";
+
+$.ajax({
+  url: movieURL,
+  method: "GET"
+}).then(function (response) {
+  console.log(response);
+  
+});
